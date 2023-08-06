54e8b1bbd1ca11a0e97f5ec884cbf47b1160ef6d
Ferraz <warley.ferraz.wf@gmail.com>
2023-08-06 20:18:58 -0300

    Changes to be committed: 	modified:   output/phonebook.exe 	modified:   phonebook.c

--- phonebook.c
+++ phonebook.c
@@ -2,23 +2,33 @@
 #include <string.h>
 
 
-typedef struct phonebook
+typedef struct
 {
-    
-};
+    char name[50];
+    char number[20];
+} person;
 
 
 int main(void)
 {
-    const char *names[] = {"Brian", "David"};
-    const char *numberes[] = {"+1-617-495-1000", "+55-94-99209-3253"};
-    int i = 0;
+    person people[2];
 
+    strcpy(people[0].name, "Brian");
+    strcpy(people[0].number, "+1-617-495-1000");
+
+    strcpy(people[1].name, "David");
+    strcpy(people[1].number, "+55-94-99209-3253");
+
+    // const char *names[] = {"Brian", "David"};
+    // const char *numberes[] = {"+1-617-495-1000", "+55-94-99209-3253"};
+    // int i = 0;
+
+    int i = 0;
     for(i = 0; i < 2; i++)
     {
-        if(strcmp(names[i], "David")==0)
+        if(strcmp(people[i].name, "David")==0)
         {
-            printf("Found %s\n", numberes[i]);
+            printf("Found %s\n", people[i].number);
             return 0;
         }
     }
